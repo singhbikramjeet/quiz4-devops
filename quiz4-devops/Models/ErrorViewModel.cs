e594e076602a1200ec89c58f3ac1d62cb70fa7c2
Bikramjeet Singh <b102@my.centennialcollege.ca>
2023-04-14 16:59:22 -0400

    adding database files

---
+++ quiz4-devops/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace quiz4_devops.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
